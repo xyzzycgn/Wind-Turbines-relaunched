84bd107c603be8c93d3aebb11e67d68a39ee19ca
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-07-15 07:29:18 +0200

    Add ordering to settings

--- settings.lua
+++ settings.lua
@@ -6,17 +6,20 @@ data:extend{
 		default_value = 1,
 		maximum_value = 10,
 		minimum_value = 0,
+		order = 'a',
 	},
 	{
 		type = 'bool-setting',
 		name = 'texugo-wind-turbine4',
 		setting_type = 'startup',
-		default_value = true
+		default_value = true,
+		order = 'b',
 	},
 	{
 		type = 'bool-setting',
 		name = 'texugo-wind-use-surface-wind-speed',
 		setting_type = 'startup',
-		default_value = true
+		default_value = true,
+		order = 'c',
 	}
 }

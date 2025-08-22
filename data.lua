e161aab873dfaa448fe15088614d6926726de775
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2025-08-22 15:33:07 +0200

    support both K2 and K2 Spaced Out

--- data.lua
+++ data.lua
@@ -10,8 +10,8 @@ if mods['bobelectronics'] then
     bobmods.lib.recipe.replace_ingredient('texugo-wind-turbine4', 'rocket-control-unit', 'electronic-processing-board')
 end
 
--- integration into krastorio2
-if mods['Krastorio2'] then
+-- integration into krastorio2 / Krastorio2 Spaced Out
+if mods['Krastorio2'] or mods['Krastorio2-spaced-out'] then
     local twt = data.raw.recipe['texugo-wind-turbine']
     for _, ingredient in pairs(twt.ingredients or {}) do
         if ingredient.name == "electronic-circuit" then

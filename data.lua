08377086d74fde1763d3d820a40f344e30f8a4ee
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-05-14 11:04:32 +0200

    removed useless code for bobelectronics (fix for #32 + #33)

--- data.lua
+++ data.lua
@@ -3,13 +3,6 @@ require 'prototypes.2_wind_generator'
 require 'prototypes.3_wind_turbine'
 require 'prototypes.4_titanic_turbine'
 
-if mods['bobelectronics'] then
-    bobmods.lib.recipe.replace_ingredient('texugo-wind-turbine', 'electronic-circuit', 'basic-circuit-board')
-    bobmods.lib.recipe.replace_ingredient('texugo-wind-turbine2', 'advanced-circuit', 'electronic-circuit-board')
-    bobmods.lib.recipe.replace_ingredient('texugo-wind-turbine3', 'processing-unit', 'electronic-logic-board')
-    bobmods.lib.recipe.replace_ingredient('texugo-wind-turbine4', 'rocket-control-unit', 'electronic-processing-board')
-end
-
 -- integration into krastorio2 / Krastorio2 Spaced Out
 if mods['Krastorio2'] or mods['Krastorio2-spaced-out'] then
     local twt = data.raw.recipe['texugo-wind-turbine']
